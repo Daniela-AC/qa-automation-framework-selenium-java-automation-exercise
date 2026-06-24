8fe5a67ba79e4327f0fb758577b22fec4e2bb28e
Daniela-AC <daniela-aguilarc@outlook.com>
2026-06-24 13:04:26 -0500

    Add Firefox and Edge browser support

--- src/main/java/com/test/automationexercise/utils/drivers/DriverFactory.java
+++ src/main/java/com/test/automationexercise/utils/drivers/DriverFactory.java
@@ -4,6 +4,10 @@ import com.test.automationexercise.utils.config.ConfigReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverFactory {
 
@@ -13,17 +17,42 @@ public class DriverFactory {
 
         String browser = config.getBrowser().toLowerCase();
 
-        switch (browser) {
-            case "chrome":
-                return new ChromeDriver(getChromeOptions());
-            default:
-                throw new IllegalArgumentException("Browser not supported: " + browser);
-        }
+        return switch (browser) {
+            case "chrome" -> new ChromeDriver(getChromeOptions());
+            case "firefox" -> new FirefoxDriver(getFirefoxOptions());
+            case "edge" -> new EdgeDriver(getEdgeOptions());
+            default -> throw new IllegalArgumentException("Browser not supported: " + browser);
+        };
     }
 
     private ChromeOptions getChromeOptions() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized", "--incognito", "--disable-popup-blocking", "--disable-notifications",
+                "--disable-blink-features=AutomationControlled", "--disable-extensions", "--ignore-certificate-errors");
+
+        if (config.isHeadless()) {
+            options.addArguments("--headless=new");
+        }
+
+        return options;
+    }
+
+    private FirefoxOptions getFirefoxOptions() {
+        FirefoxOptions options = new FirefoxOptions();
+
+        options.addArguments("-private", "--disable-popup-blocking", "--disable-notifications", "--ignore-certificate-errors");
+
+        if (config.isHeadless()) {
+            options.addArguments("--headless");
+        }
+
+        return options;
+    }
+
+    private EdgeOptions getEdgeOptions() {
+        EdgeOptions options = new EdgeOptions();
+
+        options.addArguments("--start-maximized", "--inprivate", "--disable-popup-blocking", "--disable-notifications",
                 "--disable-blink-features=AutomationControlled", "--disable-extensions", "--ignore-certificate-errors");
 
         if (config.isHeadless()) {
